db1c1d6e90b35ed4de54f75256139ee6bc2ea614
jjoar8 <jjoar8@gmail.com>
2021-02-24 15:41:59 +0900

    ScoreService

---
+++ javaworks/Java_16_ClassScore/src/com/callor/score/model/ScoreVO.java
@@ -0,0 +1,21 @@
+package com.callor.score.model;
+
+/*
+ * VO : Value Object
+ */
+
+public class ScoreVO {
+	
+	public String strName;
+	public int intKor;
+	public int intEng;
+	public int intMath;
+	public int intMusic;
+	public int intHistory;
+	
+	public int intSum;
+	public float floatAvg;
+	
+	
+
+}
