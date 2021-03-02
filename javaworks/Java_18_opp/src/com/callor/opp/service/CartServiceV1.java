ff726af25b7dbf773c00da6c90ed848b60dc797c
jjoar8 <jjoar8@gmail.com>
2021-03-02 09:41:29 +0900

    Cart

--- javaworks/Java_18_opp/src/com/callor/opp/service/CartServiceV1.java
+++ javaworks/Java_18_opp/src/com/callor/opp/service/CartServiceV1.java
@@ -20,10 +20,46 @@ import com.callor.opp.model.CartVO;
 public class CartServiceV1 {
 	
 	// CartServiceV1 클래스에 cartList 객체를 인스턴스변수로 선언
-	List<CartVO> cartList = new ArrayList<CartVO>();
+	private List<CartVO> cartList = new ArrayList<CartVO>();
+		
+	/*
+	 * inputCart() method에서 계속 사용할
+	 * 		키보드 입력을 처리할 Scanner를
+	 * 		인스턴스 영역으로 이동했다
+	 * 인스턴스 영역에 선언된 객체들은
+	 * 		특별한 경우가 아니면 private으로 일단 선언
+	 */
+	private Scanner scan = new Scanner(System.in);
 	
 	public void inputCart() {
-		Scanner scan = new Scanner(System.in);
+		/*
+		 * Scanner, File, Network 이러한 클래스들은
+		 * 운영체제와 연동하여 컴퓨터 자체의 자원(Resource)을 사용하게 된다
+		 * 컴퓨터 자체의 자원(Resource)는 유한하다
+		 * 때문에 자원은 무턱대고 열고 사용만 해서는 안된다
+		 * 자원을 사용하기 위해서 선언을 한 후
+		 * 사용이 끝나면 반드시 반납(close)해 줘야 하낟
+		 * 그래야만 Application이 실행되는 과정에서
+		 * 자원의 누수(leak)이 발생하지 않는다
+		 * 
+		 * 여기(inputCart() method)는
+		 * 사용방법에 따라서 수회 반복적으로 호출이 될 것이다
+		 * 수회 반복적으로 호출되는 동안에
+		 * Scanner가 열리고(연결) 닫히는 과정이 계속 반복된다
+		 * Resource를 열고, 닫는 과정은
+		 * 		실제 컴퓨터, 운영체제, 어플리케이션 입장에서는
+		 * 상당히 많은 일을 수행해야 한다.
+		 * 
+		 * 이러한 과정은 어플리케이션의 성능을 저하시키는
+		 * 중대한 원인이 될 수 있다
+		 * 
+		 * 그래서
+		 * Scanner를 method() 내부에서 선언하지 않고
+		 * 클래스의 인스턴스 영역으로 보낸다.
+		 * 
+		 */
+		// Scanner scan = new Scanner(System.in);
+		
 		for(int i = 0; i < 3; i++) {
 			
 			System.out.print("구매자 >> ");
